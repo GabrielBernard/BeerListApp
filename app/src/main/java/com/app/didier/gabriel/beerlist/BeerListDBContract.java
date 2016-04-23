1d2050b27b33cdf6fccb3b6019dd51ed84a624fd
Gabriel Bernard <gabriel.f.bernard@gmail.com>
2016-04-23 18:58:41 -0400

    Prémisses d'une base de données.

--- app/src/main/java/com/app/didier/gabriel/beerlist/BeerListDBContract.java
+++ app/src/main/java/com/app/didier/gabriel/beerlist/BeerListDBContract.java
@@ -4,24 +4,52 @@ package com.app.didier.gabriel.beerlist;
  * Created by Gabriel on 16-04-20.
  */
 
-import android.net.Uri;
+// import android.net.Uri;
 import android.provider.BaseColumns;
 
+/**
+ * This class defines a contract between the BeerListDatabase
+ */
 public final class BeerListDBContract {
     public static final String PACKAGE = "com.app.didier.gabriel.beerlist";
 
-    // This is a contract class, thus it must not be instantiated
+    // This prevents the class from being instantiated
     private BeerListDBContract(){}
 
+    /**
+     * Contract for the beers database.
+     */
     public static abstract class Beers implements BaseColumns {
+        // The SQL table name <Type: TEXT>
         public static final String TABLE_NAME = "beers";
+
+        // Column name for a beer's id <Type: INTEGER PRIMARY KEY >
         public static final String COLUMN_NAME_BEER_ID = "beersid";
+
+        // Column name for a beer's name <Type: TEXT>
         public static final String COLUMN_NAME_BEER_NAME = "name";
+
+        // Column name for a beer's brewery <Type: TEXT>
         public static final String COLUMN_NAME_BREWERY = "brewery";
+
+        // Column name for a beer's color <Type: TEXT>
         public static final String COLUMN_NAME_COLOR = "color";
+
+        // Column name for a beers's alcohol content <TYPE: TEXT>
         public static final String COLUMN_NAME_ALCOHOL_CONTENT = "alcohol";
+
+        // Column name for the locations where a beer can be found <Type: TEXT>
         public static final String COLUMN_NAME_WHERE_BOUGHT = "where";
+
+        // Column for the beer's score <Type: REAL>
         public static final String COLUMN_NAME_SCORE = "score";
+
+        // Column for the beer's creation timestamp <Type: INTEGER>
+        public static final String COLUMN_NAME_CREATE_DATE = "created";
+
+        // Column for the beer's modification timestamp <Type: INTEGER>
+        public static final String COLUMN_NAME_MODIFICATION_DATE = "modified";
+
     }
 }
 
--- app/src/main/java/com/app/didier/gabriel/beerlist/BeerListDatabase.java
+++ app/src/main/java/com/app/didier/gabriel/beerlist/BeerListDatabase.java
@@ -6,25 +6,18 @@ package com.app.didier.gabriel.beerlist;
 
 import com.app.didier.gabriel.beerlist.BeerListDBContract.Beers;
 
-import android.content.ClipDescription;
+
 import android.content.ContentProvider;
-import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
-import android.content.UriMatcher;
-import android.content.ContentProvider.PipeDataWriter;
-import android.content.res.AssetFileDescriptor;
-import android.content.res.Resources;
+
 import android.database.Cursor;
-import android.database.SQLException;
+
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.database.sqlite.SQLiteQueryBuilder;
+
 import android.net.Uri;
-import android.os.Bundle;
-import android.os.ParcelFileDescriptor;
-import android.provider.LiveFolders;
-import android.text.TextUtils;
+
 import android.util.Log;
 
 import java.io.FileNotFoundException;
@@ -35,16 +28,21 @@ import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 
-public class BeerListDataBase {
+public class BeerListDatabase extends ContentProvider {
 
-    // Creation of the SQLite table with the DBContract.
-    private static final String TAG = "BeerListDataBase";
+    // Database name, version, etc.
+    private static final String TAG = "BeerListDatabase";
     private static final String DATABASE_NAME = "beers.db";
-    private static final int DATABASE_VERSION = 2;
-    private static final String TEXT_TYPE = "TEXT";
-    private static final String DOUBLE_TYPE = "REAL";
+    private static final int DATABASE_VERSION = 0;
+
+    // Important data for SQLite table creation
+    private static final String TEXT_TYPE = " TEXT";
+    private static final String DOUBLE_TYPE = " REAL";
+    private static final String INTEGER_TYPE = " INTEGER";
     private static final String COMMA_SEP = ",";
-    private static final String SQL_CREATE_ENTRIES = "CREATE TABLE" + Beers.TABLE_NAME +
+
+    // Creation of the SQLite table with the DBContract
+    private static final String SQL_CREATE_ENTRIES = "CREATE TABLE " + Beers.TABLE_NAME +
             " (" + Beers._ID + " INTEGER PRIMARY KEY," +
             Beers.COLUMN_NAME_BEER_ID + TEXT_TYPE + COMMA_SEP +
             Beers.COLUMN_NAME_BEER_NAME + TEXT_TYPE + COMMA_SEP +
@@ -52,11 +50,19 @@ public class BeerListDataBase {
             Beers.COLUMN_NAME_COLOR + TEXT_TYPE + COMMA_SEP +
             Beers.COLUMN_NAME_ALCOHOL_CONTENT + DOUBLE_TYPE + COMMA_SEP +
             Beers.COLUMN_NAME_WHERE_BOUGHT + TEXT_TYPE + COMMA_SEP +
-            Beers.COLUMN_NAME_SCORE + TEXT_TYPE + COMMA_SEP + " )";
+            Beers.COLUMN_NAME_SCORE + DOUBLE_TYPE + COMMA_SEP +
+            Beers.COLUMN_NAME_CREATE_DATE + INTEGER_TYPE + COMMA_SEP +
+            Beers.COLUMN_NAME_MODIFICATION_DATE + INTEGER_TYPE + " );";
 
     private static final String SQL_DELETE_ENTRIES =
             "DROP TABLE IF EXISTS " + Beers.TABLE_NAME;
 
+    // Database helper
+    private DatabaseHelper openHelper;
+
+    /**
+     * Class that helps in creating, opening, upgrading and deleting a SQL database.
+     */
     static class DatabaseHelper extends SQLiteOpenHelper {
         DatabaseHelper(Context context) {
             super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -72,10 +78,47 @@ public class BeerListDataBase {
             Log.w(TAG, "Upgrading database from version " + oldVersion + " to "
             + newVersion + ", which will destroy all old data");
 
-            db.execSQL("DROP TABLE IF EXISTS beers");
+            db.execSQL(SQL_DELETE_ENTRIES);
 
             onCreate(db);
         }
+
+        @Override
+        public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            onUpgrade(db, oldVersion, newVersion);
+        }
+    }
+
+
+    @Override
+    public boolean onCreate() {
+        openHelper = new DatabaseHelper(getContext());
+        return true;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String where, String[] whereArgs) {
+       return 0;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues initialValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String where, String[] whereArgs) {
+     return 0;
+    }
+
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
+                        String sortOrder) {
+        return null;
     }
 }
 
